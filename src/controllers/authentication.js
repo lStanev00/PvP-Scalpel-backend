4bcb3ae0096a570bb0441ac047297df14118c2f7
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-26 19:57:21 +0200

    userId => _id

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -43,7 +43,7 @@ async function registerPost(req, res) {
         if (newUser === undefined) return;
 
         const token = jwt.sign(
-            { userId: newUser._id, email: newUser.email },
+            { _id: newUser._id, email: newUser.email },
             JWT_SECRET,
             { expiresIn: '10h' }
         );
