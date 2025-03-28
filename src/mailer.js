183188dfc77f908322d72e83baa39c219eecc3f8
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-28 13:27:15 +0200

    API email confirmation shift to 6 digits

--- src/mailer.js
+++ src/mailer.js
@@ -23,65 +23,75 @@ const transporter = nodemailer.createTransport({
 });
 
 const mail = {
-    sendJWTAuth : async function (email, token) {
-        const link = frontEndDomain + `verify/${token}`;
-        const html = `
-        <div style="
-          max-width: 600px;
-          margin: auto;
-          padding: 20px;
-          font-family: Arial, sans-serif;
-          background: #111827;
-          color: #f9fafb;
-          border-radius: 12px;
-          box-shadow: 0 0 10px rgba(0,0,0,0.3);
-        ">
-          <h2 style="color: #38bdf8;">🔒 Email Verification</h2>
-          <p style="line-height: 1.6;">
-            Thank you for registering at <strong>PvP Scalpel</strong>.<br />
-            Please click the button below to verify your email address:
-          </p>
-      
-          <div style="text-align: center; margin: 30px 0;">
-            <a href="${link}" target="_blank" style="
-              background: #38bdf8;
-              color: #000;
-              padding: 12px 24px;
-              text-decoration: none;
-              font-weight: bold;
-              border-radius: 6px;
-              display: inline-block;
-            ">Verify Email</a>
-          </div>
-      
-          <p style="font-size: 14px; color: #9ca3af;">
-            If you didn’t create an account, you can ignore this email.
-          </p>
-      
-          <hr style="border: none; border-top: 1px solid #374151; margin: 20px 0;" />
-      
-          <p style="font-size: 12px; color: #6b7280;">
-            ⚔️ Stay sharp. Fight smart.<br />
-            — The PvP Scalpel Team
-          </p>
+    sendJWTAuth : async function (email, code, option) {
+        let header
 
-          <p style="text-align: center; margin-top: 20px;">
-              <a href="${frontEndDomain}" target="_blank" style="
-                  color: #38bdf8;
-                  text-decoration: none;
-                  font-size: 13px;
-              ">
-                  🌐 Visit our website
-              </a>
-          </p>
-        </div>
+        switch (option) {
+            case "email" :
+                header = "Email Address"; break;
+            case "password": header = "Password"; break;
+            default: console.warn(`Allowed options are :\nemail\npassword`); return undefined;
+        }
+        const html = `
+                  <div style="
+                    max-width: 600px;
+                    margin: auto;
+                    padding: 20px;
+                    font-family: Arial, sans-serif;
+                    background: #111827;
+                    color: #f9fafb;
+                    border-radius: 12px;
+                    box-shadow: 0 0 10px rgba(0,0,0,0.3);
+                  ">
+                    <h2 style="color: #38bdf8;">🔒 Email Verification</h2>
+                    <p style="line-height: 1.6;">
+                      Thank you for being part of <strong>PvP Scalpel</strong>.<br />
+                      Please use the following 6-digit code to verify your ${header}:
+                    </p>
+
+                    <div style="
+                      text-align: center;
+                      margin: 30px 0;
+                      font-size: 32px;
+                      letter-spacing: 10px;
+                      font-weight: bold;
+                      background: #1f2937;
+                      padding: 16px;
+                      border-radius: 8px;
+                      display: inline-block;
+                    ">
+                      ${code}
+                    </div>
+
+                    <p style="font-size: 14px; color: #9ca3af;">
+                      If you didn’t submitted a request, you can ignore this email.
+                    </p>
+
+                    <hr style="border: none; border-top: 1px solid #374151; margin: 20px 0;" />
+
+                    <p style="font-size: 12px; color: #6b7280;">
+                      ⚔️ Stay sharp. Fight smart.<br />
+                      — The PvP Scalpel Team
+                    </p>
+
+                    <p style="text-align: center; margin-top: 20px;">
+                      <a href="${frontEndDomain}" target="_blank" style="
+                          color: #38bdf8;
+                          text-decoration: none;
+                          font-size: 13px;
+                      ">
+                          🌐 Visit our website
+                      </a>
+                    </p>
+                  </div>
+
       `;
       
 
         await transporter.sendMail({
             from: 'noreply@pvpscalpel.com',
             to: email,
-            subject: "Email verification",
+            subject: `${header} verification`,
             html: html,
         })
     }
