eac54cbb7a97614ec225bc1640d15dbfcb741599
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-28 14:50:08 +0200

    Middleware is more strickt and dont pass users with invalid sessions

--- src/middlewares/authMiddleweare.js
+++ src/middlewares/authMiddleweare.js
@@ -1,37 +1,37 @@
 import dotenv from 'dotenv';
 import validateToken from "../helpers/authToken.js";
 import User from '../Models/User.js';
+import { getOptions } from '../helpers/cookieOptions.js';
 
 const JWT_SECRET = process.env.JWT_SECRET
 export async function authMiddleware(req, res, next) {
     const auth1 = req.headers["600"];
     if (!auth1 && auth1 !== "BasicPass") return res.status(500).end();
     
-    const token = req.cookies.token
-    if(!token) return next();
-    const auth = validateToken(token, JWT_SECRET);
+    const JWT = req.cookies.token
+    if(!JWT) return next();
+    const auth = validateToken(JWT, JWT_SECRET);
 
     if(!auth){
-        req.user = undefined;
-        return next();
+        res.clearCookie("token", getOptions(req));
+        return res.status(403).end();
     }
 
     try {
         const user = await User.findById(auth._id).lean();
 
         if(!user){
-            req.user = undefined;
-            return next();
+            res.clearCookie("token", getOptions(req));
+            return res.status(403).end();
         }
 
         if (!fingerprintsMatch(auth.fingerprint, user.fingerprint)) {
-            return res.status(401).end();
+            res.clearCookie("token", getOptions(req));
+            return res.status(403).end();
         }
           
-
         req.user = user;
         next();
-
 
     } catch (error) {
         console.warn(error);
