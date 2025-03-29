d3fa68fbc58458d8e4078948dbbe44d273275c19
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-30 01:49:48 +0200

    cookies now save for localhost development requests

--- src/helpers/cookieOptions.js
+++ src/helpers/cookieOptions.js
@@ -1,18 +1,17 @@
 export function getOptions(req) {
-  const hostname = req.hostname;
-  const protocol = req.protocol;
+  const isLocalhost =
+    req.headers['ga6n1fa4fcvt'] === 'EiDcafRc45$td4aedrgh4615tokenbtw';
 
-  const isLocalhost = ["localhost", "127.0.0.1", "::1"].includes(hostname);
 
   const options = {
     httpOnly: true,
-    secure: protocol === "https", // Required for SameSite=None
-    sameSite: isLocalhost ? "Lax" : "None", // Lax for localhost dev
-    maxAge: 15 * 24 * 60 * 60 * 1000 // 15 days
+    secure: !isLocalhost,                     
+    sameSite: isLocalhost ? 'Lax' : 'None',   
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
   };
 
   if (!isLocalhost) {
-    options.domain = ".pvpscalpel.com"; // Enable cross-subdomain access
+    options.domain = '.pvpscalpel.com';
   }
 
   return options;
