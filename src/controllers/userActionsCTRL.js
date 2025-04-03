fd379c40331073fcc9ebb19722c86dbb454d247f
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-03 12:07:47 +0300

    Spacing

--- src/controllers/userActionsCTRL.js
+++ src/controllers/userActionsCTRL.js
@@ -16,20 +16,20 @@ async function setLike(req, res) {
     if (!charID) return jsonMessage(res, 400, "Bad request");
   
     try {
-      const alreadyLiked = await Char.findOne({ _id: charID, likes: user._id });
+        const alreadyLiked = await Char.findOne({ _id: charID, likes: user._id });
   
-      const update = alreadyLiked
-        ? { $pull: { likes: user._id } }
-        : { $addToSet: { likes: user._id } };
+        const update = alreadyLiked
+            ? { $pull: { likes: user._id } }
+            : { $addToSet: { likes: user._id } };
   
-      const updatedChar = await Char.findByIdAndUpdate(charID, update, { new: true });
+        const updatedChar = await Char.findByIdAndUpdate(charID, update, { new: true });
   
-      return res.status(200).json({ likes: updatedChar.likes.length });
+        return res.status(200).json({ likes: updatedChar.likes.length });
     } catch (error) {
-      console.warn(error);
-      return jsonMessage(res, 500, "Internal Server Error");
+        console.warn(error);
+        return jsonMessage(res, 500, "Internal Server Error");
     }
-  }
+}
   
 
 export default userActionCTRL;
