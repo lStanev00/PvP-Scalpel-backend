008e20c727ddb4e2c1b12fc87715b4900fbe0913
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-28 15:53:12 +0300

    now filters the members that are no longer in the guild and prepares to delete em

--- src/querry/membersPatch.js
+++ src/querry/membersPatch.js
@@ -5,8 +5,15 @@ import { getGuildMembers } from "../helpers/testPatchModul.js";
 export async function membersPatch() {
     
     const memberList = await getGuildMembers();
-    
     const dbMemberList = await Member.find({ _id, blizID });
+    const dbBlizIDs = dbMemberList.filter(entry => entry.blizID);
+    const mapIds = new Map(dbBlizIDs);
+
+    const falsyIds = Array.from(memberList.filter(entry => {
+        const check = mapIds.get(entry.character.id);
+        return !check ? true : false;
+    } ));
+
     
 }
 
