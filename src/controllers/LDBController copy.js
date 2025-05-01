67e300d51c27fc18d1209bc6deb250a0f71544b9
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-01 10:54:12 +0300

    new fn for sending json data

--- src/controllers/LDBController copy.js
+++ src/controllers/LDBController copy.js
@@ -45,12 +45,9 @@ async function soloGet(req,res) {
 }
 async function blitzGet(req,res) {
     try {
-        let players = await Member.find(
-            { "rating.solo_bg": { $exists: true } }, 
-            { name: 1, "rating.solo_bg": 1,'playerRealmSlug': 1, "achieves.BG": 1, "media.avatar": 1, 'class' : 1, 'spec': 1, _id: 1 } 
-          );
-        players = sortBlitz(players);
-        res.status(200).json(players);
+        const charList = await findRatingAndSort(`blitz`);
+
+        return jsonMessage( res, 200, charList );
         
     } catch (error) {
         res.status(404);
@@ -58,13 +55,9 @@ async function blitzGet(req,res) {
 }
 async function BGGet(req,res) {
     try {
-        let players = await Member.find(
-            { "rating.rbg": { $exists: true } }, 
-            { name: 1, "rating.rbg": 1,  'class' : 1, 'playerRealmSlug': 1, 'spec': 1, "media.avatar": 1,  "achieves.BG": 1, _id: 1 } 
-          );
-          
-        players = sortBG(players);
-        res.status(200).json(players);
+        const charList = await findRatingAndSort(`rbg`);
+
+        jsonMessage(res, 200, charList)
         
     } catch (error) {
         res.status(404);
@@ -118,51 +111,6 @@ function sortBlitz(players) {
 
     return players
 }
-function sortBG(players) {
-    players = players.sort((a, b) => {
-        // 1. Primary Criteria: bg rating
-        const ratingA = b.rating?.["rbg"] || 0;
-        const ratingB = a.rating?.["rbg"] || 0;
-    
-        if (ratingA !== ratingB) {
-            return ratingA - ratingB; // Higher solo_bg rating comes first
-        }
-    
-        // 2. Secondary Criteria: Achievements
-        const getPriority = (char) => {
-            // Check for Hero of the Alliance/Horde
-            const heroAchieve = char.achieves?.["BG"]?.find(({ name }) =>
-                ["Hero of the Alliance", "Hero of the Horde"].includes(name)
-            );
-    
-            if (heroAchieve) return Infinity; // Hero achievements have the highest priority
-    
-            // Check for "Earn a rating of X in Rated BG/Blitz"
-            const ratingAchieve = char.achieves?.["BG"]?.find(({ description }) =>
-                description.includes("Earn a rating of")
-            );
-    
-            if (ratingAchieve) {
-                const ratingMatch = ratingAchieve.description.match(/Earn a rating of (\d+)/);
-                return ratingMatch ? parseInt(ratingMatch[1], 10) : 0; // Extract and return the numeric rating
-            }
-    
-            return 0; // Default priority if no matching achievements
-        };
-    
-        const priorityA = getPriority(a);
-        const priorityB = getPriority(b);
-    
-        if (priorityA !== priorityB) {
-            return priorityB - priorityA; // Higher priority (rating or Hero) comes first
-        }
-    
-        return 0; // If everything is equal, maintain original order
-    });
-
-    return players
-}
-
 
 // Helper for the fetches and sort
 
@@ -170,7 +118,9 @@ async function findRatingAndSort (bracket) {
 
     if (bracket == "shuffle" || bracket == "blitz") {
         try {
-            const charList = await Char.find({ guildMember:true }).lean();
+            const charList = await Char.find({ guildMember:true })
+            .select(`name playerRealm race class activeSpec rating achieves media server`)
+            .lean();
             const shuffleEntries = charList.filter(entry => {
                 const ratingList = Object.entries(entry?.rating);
                 let result = []
@@ -190,7 +140,6 @@ async function findRatingAndSort (bracket) {
                     });
                 }
 
-                delete entry.gear
                 entry.rating = {
                     [result[0][0]] : result[0][1]
                 }
@@ -209,7 +158,9 @@ async function findRatingAndSort (bracket) {
         const charList = await Char.find({
             [`rating.${bracket}.currentSeason.rating`] : { $exists: true },
             guildMember: true
-        }).sort({
+        })
+        .select(`name playerRealm race class activeSpec rating achieves.${bracket == "shuffle" ? "solo" : "Blitz"} media server`)
+        .sort({
             [`rating.${bracket}.currentSeason.rating`]: -1,
             [`rating.${bracket}.record`]: -1
         })
--- src/helpers/resposeHelpers.js
+++ src/helpers/resposeHelpers.js
@@ -1,4 +1,8 @@
 export function jsonMessage(res, status, msg) {
     return res.status(status).json({ message: msg });
-  }
+}
+  
+export function jsonResponse(res, status, data) {
+    return res.status(status).json(data);
+}
   
