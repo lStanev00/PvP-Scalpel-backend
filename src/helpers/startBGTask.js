e4cf3c16f405569530ee34e0dbc7c6409e714589
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-19 16:41:20 +0300

    Getting ready to emb the repetitive updates over time into the api so the app wont need 2nd terminal for it

---
+++ src/helpers/startBGTask.js
@@ -0,0 +1,17 @@
+export function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function startBackgroundTask(fn, ms) {
+  (async () => {
+    while (true) {
+      console.time(`${fn.name}`)
+      const success = await fn();
+      if (success) {
+        timeNow = getBGTime();
+        console.timeEnd(`${fn.name}`)
+      }
+      await delay(ms);
+    }
+  })();
+}
