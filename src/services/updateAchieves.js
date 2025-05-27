90c522cd03f2422561ac6a88d702d0c957b75c8d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-27 15:27:40 +0300

    Getting token

--- src/services/updateAchieves.js
+++ src/services/updateAchieves.js
@@ -1,4 +1,18 @@
 import dotenv from 'dotenv';
+import helpFetch from '../helpers/blizFetch-helpers/endpointFetchesBliz';
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
+
+const buildHeaders = async () => {
+    let accessToken = await helpFetch.getAccessToken(clientId, clientSecret);
+
+    const headers = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`, 
+          'Cache-Control': 'no-cache',  
+          'Pragma': 'no-cache',
+        },
+    }
+
+}
