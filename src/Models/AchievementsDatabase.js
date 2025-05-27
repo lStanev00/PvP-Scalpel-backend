63aeba920e657302a7ef5a1c78089cac8f805a20
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-27 15:12:30 +0300

    New AchievementsDatabase Models

---
+++ src/Models/AchievementsDatabase.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const achSchema = new mongoose.Schema({
+    _id: Number, // or ObjectId, Number, etc., depending on your custom ID type
+    name: String,
+    key: String
+});
+
+const AchModel = mongoose.model('Achievement', achSchema);
