ea8be1647b07ccfd9280db2adee46f454ef5b1ac
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-28 10:30:33 +0300

    More info to the model

--- src/Models/Achievements.js
+++ src/Models/Achievements.js
@@ -3,7 +3,21 @@ import mongoose from "mongoose";
 const achSchema = new mongoose.Schema({
     _id: Number, 
     name: String,
-    href: String
+    href: String,
+    media: String,
+    description: String,
+    displayOrder: { type: Number, index: true },
+    category: {
+        id: { type: Number, index: true },
+        name: String,
+    },
+    criteria: { type: Number, index: true, unique: true },
+    expansion: {
+        name: String,
+        season: Number,
+        required: false
+    }
+
 });
 
 const Achievement = mongoose.model('Achievement', achSchema);
