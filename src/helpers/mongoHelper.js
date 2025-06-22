7252e9a1081d7e8853148423b084290276dfa60b
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-22 10:09:37 +0300

    prep for cloud

--- src/helpers/mongoHelper.js
+++ src/helpers/mongoHelper.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
-const dbName = `PvP-Scalpel`
-const uri = `mongodb://127.0.0.1:27017/${dbName}`
+// const dbName = `PvP-Scalpel`
+// const uri = `mongodb://127.0.0.1:27017/${dbName}`
+import dotenv from 'dotenv';
+dotenv.config({path: "../../.env"});
+const MONGODB_CONNECTION= process.env.MONGODB_CONNECTION
 
 export async function DBconnect() {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(MONGODB_CONNECTION);
         console.log("MongoDB Connected Successfully!");
     } catch (err) {
         console.error("MongoDB Connection Error:", err.message);
