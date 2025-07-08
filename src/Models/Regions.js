d496b49abb4cc36397076150af3c8c003bb087ea
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-07-08 18:01:03 +0300

    new regions model

---
+++ src/Models/Regions.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const regionSchema = new mongoose.Schema({
+    _id: Number,
+    name: {
+        type: String,
+        enum: ['EU', 'US', 'KR', 'TW', "CN"],
+        index: true
+    }
+}, {_id: false});
+
+const Region = mongoose.model('Region', regionSchema);
+
+export default Region;
