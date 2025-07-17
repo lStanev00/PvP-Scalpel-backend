403bf28a714443fbf67e00db6c6f6dbe48bfd9f6
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-07-17 11:29:38 +0300

    trycatch

--- src/helpers/slugify.js
+++ src/helpers/slugify.js
@@ -1,6 +1,11 @@
 export default function slugify (plainText) {
-    if (typeof plainText !== "string") throw new TypeError(`The input is of type ${typeof plainText}, while it's expected to be a string`);
-
-    const output = (plainText.toLowerCase()).trim().replaceAll(` `, "-");
-    return output
+    try {
+        if (typeof plainText !== "string") throw new TypeError(`The input is of type ${typeof plainText}, while it's expected to be a string`);
+    
+        const output = (plainText.toLowerCase()).trim().replaceAll(` `, "-");
+        return output
+        
+    } catch (error) {
+        return undefined
+    }
 }
