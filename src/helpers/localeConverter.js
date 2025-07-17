e3bd1d2103f9e9cdbd5cc6a15b1b52502a9cbaa4
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-07-17 11:29:32 +0300

    bigfix

--- src/helpers/localeConverter.js
+++ src/helpers/localeConverter.js
@@ -1,4 +1,4 @@
 export default function convertLocale(plainLocale) {
-    const fixed = locale.replace(/^([a-z]{2})([A-Z]{2})$/, "$1_$2"); 
+    const fixed = plainLocale.replace(/^([a-z]{2})([A-Z]{2})$/, "$1_$2"); 
     return fixed
 }
