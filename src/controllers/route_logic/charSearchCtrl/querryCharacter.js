d683a022ea6d6d749c9f0b0f4ff84297b16715a7
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-07-18 13:08:53 +0300

    extract all possible realms that match the search string

--- src/controllers/route_logic/charSearchCtrl/querryCharacter.js
+++ src/controllers/route_logic/charSearchCtrl/querryCharacter.js
@@ -3,12 +3,15 @@ import { searchCharFromMap } from "../../../caching/searchCache/charSearchCache.
 import { searchRealmFromMap } from "../../../caching/searchCache/realmSearchCach.js";
 import convertSearch from "../../../helpers/convertSearch.js";
 
-export default async function queryCharacterBySearch(search) {
+export default function queryCharacterBySearch(search) {
     if (typeof search !== "string") {
         console.warn(search + "'s not a string!");
         return undefined
     }
-    let [name, realm, server] = convertSearch(search);
+
+    const realms = buildRealmsNamesByCharSrch(search);
+
+    return realms
 
     const result = {
         exactMatch: undefined,
@@ -33,27 +36,42 @@ export default async function queryCharacterBySearch(search) {
 
 }
 
-export function buildRealmNameByCharSrch (charMatchArr) {
-    if (!(Array.isArray(charMatchArr))) {
-        console.warn(charMatchArr + "'s not an Array type!");
+export function buildRealmsNamesByCharSrch (charSearch) {
+    if (typeof charSearch !== "string") {
+        console.warn(charSearch + "'s not a string!");
         return undefined
     }
 
-    const result = {
+    const [name, realm, server] = convertSearch(charSearch);
 
+    const realmSearchMatches = searchRealmFromMap(realm);
+    if (!realmSearchMatches) return undefined;
+    const serverMatch = (searchRegionFromMapBySlug(server))[0];
+
+    const realmSlugMatch = realmSearchMatches?.relRealms.filter(entry => entry.region == serverMatch);
+
+    const realms = [];
+
+    for (const entry of realmSlugMatch) {
+        const result = {
+            _id: entry._id,
+            server: server,
+            slug: entry?.slug,
+            
+        }
+        const realmNames = entry?.["name"];
+        const realmLocale = entry?.["locale"];
+
+        const langCheckName = realmNames?.[realmLocale];
+        const enGbCheck = realmNames?.["en_GB"];
+        
+        if (langCheckName !== undefined && enGbCheck !== undefined) {
+            result.name = langCheckName === enGbCheck ? langCheckName : `${langCheckName} (${enGbCheck})`;
+        } else if (enGbCheck !== undefined) result.name =  enGbCheck
+
+        realms.push(result)
     }
 
-    for (const search of charMatchArr) {
-
-        if(typeof search !== "string") continue;
-
-        const [name, realm, server] = convertSearch(search);
-
-        const serverMatch = searchRegionFromMapBySlug(server);
-
-        console.log(serverMatch);
-
-    }
-    
+    return realms
 
 }
