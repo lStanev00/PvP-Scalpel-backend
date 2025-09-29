010ff116ccdacdc344193fdcb04fde750f641883
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-09-29 10:24:02 +0300

    services are exported to modul and booted in it

--- DBMS.js
+++ DBMS.js
@@ -4,14 +4,10 @@ import { DBconnect } from "./src/helpers/mongoHelper.js";
 import router from "./src/router.js";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
-import { delay, startBackgroundTask } from "./src/helpers/startBGTask.js";
-import { updateGuildMembersData } from "./src/services/PatchGuildMembersData.js";
-import updateDBAchieves from "./src/services/updateAchieves.js";
 import { corsOptions, productionUrl } from "./src/corsSetup.js";
-import updateDBRealms from "./src/services/updateRealms.js";
-import initialCache from "./src/caching/initialCache.js";
 import sanitizer from "./src/middlewares/sanitizer.js";
 import compression from "compression";
+import startServices from "./src/services/servicesMain.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -30,12 +26,6 @@ app.use(express.json({ extended: false }));
 app.use("/", sanitizer);
 app.use(`/`, router);
 
+await startServices();
 
 app.listen(port, console.info(`Server's running at http://localhost:${port} or ${productionUrl}`));
-
-await initialCache();
-startBackgroundTask(updateDBRealms, 2147483647); // max 24.8 days
-await delay(3000);
-startBackgroundTask(updateGuildMembersData, 3600000); // 1 hr
-await delay(3000);
-startBackgroundTask(updateDBAchieves, 604800000); // 1 week
---
+++ src/services/servicesMain.js
@@ -0,0 +1,17 @@
+import initialCache from "../caching/initialCache.js";
+import { delay, startBackgroundTask } from "../helpers/startBGTask.js";
+import { updateGuildMembersData } from "./PatchGuildMembersData.js";
+import updateDBAchieves from "./updateAchieves.js";
+import updateDBRealms from "./updateRealms.js";
+
+export default async function startServices() {
+    const clearenceMS = 3000;
+
+    await initialCache();
+    startBackgroundTask(updateDBRealms, 2147483647); // max 24.8 days
+    await delay(clearenceMS);
+    startBackgroundTask(updateGuildMembersData, 3600000) // 1 hr
+    await delay(clearenceMS);
+    startBackgroundTask(updateDBAchieves, 604800000) // 1 week
+
+}
