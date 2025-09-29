1ac9e36cc93bc6896b4e48cfa49c008cfe64fbb6
lStanev00 <lychezarcho@abv.bg>
2025-09-29 20:37:59 +0300

    realmCache mock refactoring to use Redis instead of inMem cache

--- src/caching/realms/realmCache.js
+++ src/caching/realms/realmCache.js
@@ -1,18 +1,21 @@
 import { EventEmitter } from "events";
 import Realm from "../../Models/Realms.js";
+import getCache from "../../helpers/redis/getterRedis.js";
+import setCache from "../../helpers/redis/setterRedis.js";
 
 const emitter = new EventEmitter();
+// let realmIdsMap = new Map();
 
-let realmIdsMap = new Map();
-
-export function getRealmIdsMap() {
-    return realmIdsMap
+export async function getRealmIdsMap() {
+    // return realmIdsMap
+    return await getCache("Realms");
 }
 
-export function findRealmById(id) {
+export async function findRealmById(id) {
     if(!id) return
 
-    return realmIdsMap.get(id);
+    // return realmIdsMap.get(id);
+    return await getCache(id, "Realms");
     
 }
 
@@ -20,7 +23,7 @@ export async function setRealmIdsMap() {
     const newMap = await mapDBRealms()
 
     if(newMap !== null){
-        realmIdsMap = newMap;
+        // realmIdsMap = newMap;
         emitter.emit('update', newMap);
     }
 
@@ -30,7 +33,7 @@ export async function initialSetRealmIdsMap() {
     const newMap = await mapDBRealms()
 
     if(newMap !== null){
-        realmIdsMap = newMap;
+        // realmIdsMap = newMap;
     }
 
 }
@@ -52,6 +55,7 @@ export async function mapDBRealms () {
             const value = entry;
 
             shadowMap.set(String(key), value);
+            await setCache(key, value, "Realms");
 
         }
 
