7f4a3d9db5a6cb15dcb095dd7eab04bd5b3099e8
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-09-29 14:08:53 +0300

    renamings and clearups

--- src/helpers/connectRedis.js
+++ src/helpers/connectRedis.js
@@ -1,25 +1,25 @@
 import { createClient } from "redis";
-import dotenv from 'dotenv';
 
+
+const isLocal = process.env.REDIS_PUBLIC_URL;
+let url = `redis://default:${process.env.REDISPASSWORD}@${process.env.REDISHOST}:${process.env.REDISPORT}`;
+if (isLocal !== undefined) {
+    url = isLocal;
+}
+const redisCache = createClient({
+    url: url
+});
 
 export default async function connectRedis() {
-    const isLocal = process.env.REDIS_PUBLIC_URL;
-    let url = `redis://default:${process.env.REDISPASSWORD}@${process.env.REDISHOST}:${process.env.REDISPORT}`;
-    if (isLocal !== undefined) {
-        url = isLocal;
-    }
-    let client = createClient({
-        url: url
-    });
 
     if(isLocal)
 
-    await client.connect();
+    await redisCache.connect();
 
     const tryout = new Map();
-    await client.set("mapTest", JSON.stringify(tryout));
+    await redisCache.set("mapTest", JSON.stringify(tryout));
 
-    const value = await client.get("mapTest");
+    const value = await redisCache.get("mapTest");
 
     console.info("Redis get try == " + value);
 
