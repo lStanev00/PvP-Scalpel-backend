9035f46031c264944319c8d707e4aff1028363ff
lStanev00 <lychezarcho@abv.bg>
2025-09-30 06:33:48 +0300

    Install local redis on my linux

--- src/helpers/redis/connectRedis.js
+++ src/helpers/redis/connectRedis.js
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 import { configDotenv } from "dotenv";
 configDotenv()
 
-const isLocal = process.env.REDIS_PUBLIC_URL;
+const isLocal = process.env.REDIS_LOCAL_URL;
 let url = `redis://default:${process.env.REDISPASSWORD}@${process.env.REDISHOST}:${process.env.REDISPORT}`;
 
 if (isLocal !== undefined) {
