2e29b5c8b8b894baad36dae0daa1212aa2b465c7
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-10-09 17:01:16 +0300

    new schema to record the results for past weeks

---
+++ src/Models/WeeklyWinnersRecord.js
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const weeklyWinnersRecordSchema = new Schema({
+    blitz: [{ type: { playerSearch: String, result: Number }, default: [] }],
+    "2v2": [{ type: { playerSearch: String, result: Number }, default: [] }],
+    "3v3": [{ type: { playerSearch: String, result: Number }, default: [] }],
+    ss: [{ type: { playerSearch: String, result: Number }, default: [] }],
+    RBG: [{ type: { playerSearch: String, result: Number }, default: [] }]
+}, {
+    timestamps: true
+});
+
+const WeeklyWinnersRecord = model("WeeklyWinnersRecord", weeklyWinnersRecordSchema);
+export default WeeklyWinnersRecord;
