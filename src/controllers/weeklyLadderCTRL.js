003ce32ed1fae7757b94508f439ef55826e4f35f
lStanev00 <lychezarcho@abv.bg>
2025-10-14 15:56:57 +0300

    implementing route and error handling fallbacks

--- src/controllers/weeklyLadderCTRL.js
+++ src/controllers/weeklyLadderCTRL.js
@@ -1,18 +1,40 @@
 import { Router } from "express";
-import { getTop10ForABracket } from "../caching/weeklyChamps/weeklyChampsCache.js";
+import { getTop10ForABracket, getFullWeekly } from "../caching/weeklyChamps/weeklyChampsCache.js";
 import { jsonResponse } from "../helpers/resposeHelpers.js";
 
 const weeklyLadderCTRL = Router();
 
+weeklyLadderCTRL.get("/weekly", getAllWeekly);
 weeklyLadderCTRL.get("/weekly/:bracket", getWeekly);
 
 async function getWeekly(req, res) {
 
-    const { bracket } = req.params;
-    const data = await getTop10ForABracket(bracket);
-    if (!data || data === 404) return jsonResponse(res, 404, "Check the bracket param");
+    try {
+        
+        const { bracket } = req.params;
+        const data = await getTop10ForABracket(bracket);
+        if (!data || data === 404) return jsonResponse(res, 404, "Check the bracket param");
+    
+        // res.set("Cache-Control", "public, max-age=1000");
+        return jsonResponse(res, 200, data);
+    } catch (error) {
+        console.error(error);
+        jsonResponse(res, 500), "Unexpected error has occured. Please report it.";
+    }
 
-    return jsonResponse(res, 200, data);
+}
+
+async function getAllWeekly(req, res) {
+    try {
+        const data = await getFullWeekly();
+        if(data === 404) return jsonResponse(res, 404, "Fail to retrive data");
+
+        // res.set("Cache-Control", "public, max-age=1000");
+        return jsonResponse(res, 200, data);
+    } catch (error) {
+        console.error(error);
+        jsonResponse(res, 500), "Unexpected error has occured. Please report it.";
+    }
 }
 
 export default weeklyLadderCTRL;
