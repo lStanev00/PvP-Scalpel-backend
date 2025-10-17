0babb090a0dab403f11f1d9dbec0e1e5ca7064da
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-10-17 13:38:38 +0300

    opening the worker to tasks so main thread goes brr

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -2,6 +2,8 @@ import startServices from "../services/servicesMain.js"
 import { DBconnect } from "../helpers/mongoHelper.js";
 import connectRedis from "../helpers/redis/connectRedis.js";
 import { delay } from "../helpers/startBGTask.js";
+import { parentPort } from "node:worker_threads";
+import { CharSearchCacheEmiter } from "../caching/searchCache/charSearchCache.js";
 
 (async () => {
     await DBconnect(true);
@@ -11,3 +13,15 @@ import { delay } from "../helpers/startBGTask.js";
     
     await startServices();
 })()
+
+parentPort.on('message', (msg) => {
+    console.log('Worker received:', msg);
+
+    if (msg.type === 'purge') {
+        // do your wipeCharSearchEntry(msg.payload)
+        const msgArgs = msg.payload;
+        if (!(Array.isArray(msgArgs))) return;
+        CharSearchCacheEmiter.emit("purge", ...msgArgs)
+        parentPort.postMessage({ type: 'done', result: 200 });
+    }
+});
