67f5fc2b69fa0cada488b44050bbf868274db3ad
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-10-17 19:43:42 +0300

    introduce richer char datas

--- src/caching/weeklyChamps/weeklyChampsCache.js
+++ src/caching/weeklyChamps/weeklyChampsCache.js
@@ -3,10 +3,12 @@ import getCache, { hashGetAllCache } from "../../helpers/redis/getterRedis.js";
 import setCache from "../../helpers/redis/setterRedis.js";
 import formatWeeklyData from "../../services/Service-Helpers/updateWeeklyLadder/formatWeeklyData.js";
 import { EventEmitter } from "node:events";
+import { getCharacter } from "../characters/charCache.js";
+import convertSearch from "../../helpers/convertSearch.js";
 
 export const WeeklyEmitter = new EventEmitter();
 const hashName = "weeklyChampsCache";
-const humanReadableName = "Weekly"
+const humanReadableName = "Weekly";
 
 WeeklyEmitter.on("update", (msg) => console.log(`[${humanReadableName}] ${msg}`));
 WeeklyEmitter.on("error", (msg) => console.error(`[${humanReadableName} ERROR] ${msg}`));
@@ -23,10 +25,27 @@ export async function cacheWeeklyData(data = undefined) {
     await delCache(hashName);
 
     for (const [bracketName, bracketData] of Object.entries(data)) {
+        if (bracketData.length !== 0)
+            for (const [index, value] of Object.entries(bracketData)) {
+                const key = value?.playerSearch;
+                if (!key) continue;
+                const CSParts = convertSearch(key);
+                if (CSParts?.length !== 3) continue;
+                const charData = await getCharacter(CSParts[2], CSParts[1], CSParts[0], false);
+                if (!charData || charData === 404) {
+                    WeeklyEmitter.emit("info", `Skipped invalid char for key: ${key}`);
+                    continue;
+                }
+                value.name = charData?.name;
+                value.media = charData?.media;
+                value.activeSpec = charData?.activeSpec;
+                value.class = charData?.class;
+                bracketData[index] = value;
+            }
         await setCache(bracketName, bracketData, hashName);
     }
 
-    await setCache("lastUpdated", Date.now(), hashName); // set the last updated timestamp 
+    await setCache("lastUpdated", Date.now(), hashName); // set the last updated timestamp
 
     WeeklyEmitter.emit("update", "Just cached the data for all brackets");
 }
@@ -48,18 +67,18 @@ export async function getTop10ForABracket(bracketName) {
     if (!bracketData) return 404;
 
     const result = {
-        data: bracketData.slice(0,10),
-        lastUpdated
-    }
+        data: bracketData.slice(0, 10),
+        lastUpdated,
+    };
 
-    return result
+    return result;
 }
 
 export async function getFullWeekly() {
     const data = await hashGetAllCache(hashName);
     if (!data) {
         WeeklyEmitter.emit("error", "Fail to retrive data at getFullWeekly function!");
-        return 404
+        return 404;
     }
     return data;
 }
