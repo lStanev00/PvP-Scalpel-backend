cf6e5857a46f0e494a4f65048a5c538205ad9a0d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-10-18 05:17:20 +0300

    invoke the new workier and deprecate old logic

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,27 +1,16 @@
 import startServices from "../services/servicesMain.js"
 import { DBconnect } from "../helpers/mongoHelper.js";
 import connectRedis from "../helpers/redis/connectRedis.js";
+import startRedisCharSubscriber from "../helpers/redis/charSubscriber.js";
 import { delay } from "../helpers/startBGTask.js";
-// import { parentPort } from "node:worker_threads";
-// import { CharSearchCacheEmiter } from "../caching/searchCache/charSearchCache.js";
 
 await DBconnect(true);
 await connectRedis(true);
+await startRedisCharSubscriber();
+
 (async () => {
 
     await delay(5000);
     
     await startServices();
 })()
-
-// parentPort.on('message', (msg) => {
-//     console.log('Worker received:', msg);
-
-//     if (msg.type === 'purge') {
-//         // do your wipeCharSearchEntry(msg.payload)
-//         const msgArgs = msg.payload;
-//         if (!(Array.isArray(msgArgs))) return;
-//         CharSearchCacheEmiter.emit("purge", ...msgArgs)
-//         parentPort.postMessage({ type: 'done', result: 200 });
-//     }
-// });
