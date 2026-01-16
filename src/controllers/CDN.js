6fe958bc4a2dc62823bee5de810a26d8a87c52e1
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-01-16 14:21:36 +0200

    finalize controller

--- src/controllers/CDN.js
+++ src/controllers/CDN.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { Router } from "express";
 import { jsonResponse } from "../helpers/resposeHelpers.js";
 import formManifest from "./route_logic/CDN/getManifest.js";
+import newManifest from "./route_logic/CDN/postManifest.js";
 
 const AUTH = process.env.JWT_CDN_PUBLIC;
 
@@ -12,30 +13,13 @@ if (!AUTH) {
 const CDNCTRL = Router();
 
 CDNCTRL.get("/CDN/getManifest", getManifest);
+CDNCTRL.post("/CDN/postManifest", postManifest);
 
 /**
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  */
-async function getManifest(req, res) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-        return jsonResponse(res, 401, { error: "Missing Authorization header" });
-    }
-
-    const parts = authHeader.split(" ");
-
-    if (parts.length !== 2) {
-        return jsonResponse(res, 401, { error: "Malformed Authorization header" });
-    }
-
-    const [type, token] = parts;
-
-    if (type !== "Bearer" || token !== AUTH) {
-        return jsonResponse(res, 403, { error: "Invalid token" });
-    }
-
+async function getManifest(_, res) {
     const data = await formManifest();
 
     if (data === null) {
@@ -43,6 +27,40 @@ async function getManifest(req, res) {
     }
 
     return jsonResponse(res, 200, data);
+}
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ */
+async function postManifest(req, res) {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return jsonResponse(res, 401, { error: "Missing Authorization header" });
+        }
+
+        const parts = authHeader.split(" ");
+
+        if (parts.length !== 2) {
+            return jsonResponse(res, 401, { error: "Malformed Authorization header" });
+        }
+
+        const [type, token] = parts;
+
+        if (type !== "Bearer" || token !== AUTH) {
+            return jsonResponse(res, 403, { error: "Invalid token" });
+        }
+
+        const ok = await newManifest(req.body);
+
+        if (!ok) return jsonResponse(res, 400, { error: "Invalid manifest payload" });
+
+        return jsonResponse(res, 201, { success: true });
+    } catch (error) {
+        return jsonResponse(res, 500);
+    }
 }
 
 export default CDNCTRL;
