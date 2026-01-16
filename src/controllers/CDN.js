b01f1e2918201006e7dbbdff1b99d4c2657a8a1d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-01-16 13:54:42 +0200

    further controller draft

--- src/controllers/CDN.js
+++ src/controllers/CDN.js
@@ -1,8 +1,23 @@
+import "dotenv/config";
 import { Router } from "express";
+import { jsonResponse } from "../helpers/resposeHelpers.js";
+
+const AUTH = process.env.JWT_CDN_PUBLIC;
 
 const CDNCTRL = Router();
 
-CDNCTRL.get("CDN/getManifest", console.log("Hello CDN"));
-// CDNCTRL
-// CDNCTRL
-// CDNCTRL
+CDNCTRL.get("/CDN/getManifest", getManifest);
+
+async function getManifest(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return jsonResponse(res, 401, { error: "Missing Authorization header" });
+
+    const [type, token] = authHeader.split(" ");
+
+    if (type !== "Bearer" || token !== AUTH)
+        return jsonResponse(res, 403, { error: "Invalid token" });
+
+    return res.json({ message: "Hello CDN" });
+}
+
+export default CDNCTRL;
