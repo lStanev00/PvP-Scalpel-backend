397eb92f9cbd7cdccb98898bdc8e9eb66fc0774b
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-01-17 12:43:08 +0200

    new caching for the manifest

---
+++ src/caching/CDNCache/manifastCache.js
@@ -0,0 +1,44 @@
+import { EventEmitter } from "events";
+import getCache from "../../helpers/redis/getterRedis.js";
+import setCache from "../../helpers/redis/setterRedis.js";
+import pullManifest from "./CDN/pullManifest.js";
+
+const key = "cdn:manifest";
+
+const emitter = new EventEmitter();
+emitter.on("update", () => console.info(`[${key} Cache] ${key} just got cached`));
+
+export const initialSetManifestIdsMap = async () => await storeNewManifest();
+
+export async function getManifest() {
+    try {
+        const cachedManifest = await getCache(key);
+        if (cachedManifest !== null || (cachedManifest !== undefined && cachedManifest))
+            return cachedManifest;
+
+        const manifest = await storeNewManifest();
+        if (manifest !== null) return manifest;
+    } catch (error) {}
+}
+
+export async function storeNewManifest() {
+    try {
+        const manifest = await pullManifest();
+
+        if (manifest !== null) {
+            await setCache(key, manifest, "", 3600);
+            emitter.emit("update");
+            return manifest;
+        }
+
+        console.warn(
+            "There was a problem caching the manifest ===" +
+                manifest +
+                " \nType: " +
+                typeof manifest
+        );
+        return null;
+    } catch (error) {
+        console.error(error);
+    }
+}
