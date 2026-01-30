6f9ca5720a511dc2dbd84d78625ef3e422ece8bb
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-01-30 11:04:22 +0200

    auto populate specs

--- src/Models/GameClass.js
+++ src/Models/GameClass.js
@@ -1,21 +1,38 @@
 import { model, Schema } from "mongoose";
 
-const GameClassModel = new Schema({
-    _id: Number,
-    name: {
-        type: String,
-        required: [true, "Name is required"],
+const GameClassModel = new Schema(
+    {
+        _id: Number,
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        media: {
+            type: String,
+            required: [true, "Media string href is required"],
+        },
     },
-    media: {
-        type: String,
-        required: [true, "Media string href is required"],
-    },
-    role: {
-        type: String,
-        enum: ["tank", "damage", "heal"],
-        required: [true, "Role type is required: tank, damage, heal"],
-    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+GameClassModel.virtual("specs", {
+    ref: "GameSpecialization",
+    localField: "_id",
+    foreignField: "relClass",
+    justOne: false,
 });
+
+GameClassModel.pre(/^find/, function (next) {
+    this.populate("specs");
+    next();
+});
+
+
+GameClassModel.set("toObject", {  virtuals: true  });
+GameClassModel.set("toJSON", {  virtuals: true  });
 
 
 const GameClass = model("GameClass", GameClassModel);
