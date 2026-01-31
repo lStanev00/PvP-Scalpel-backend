95a0c1f5173d88a4d4246e68e6bec9fe6b1adc04
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-01-31 11:27:48 +0200

    semantic update

--- src/controllers/route_logic/gameDataCTRL/retriveValidSpells.js
+++ src/controllers/route_logic/gameDataCTRL/retriveValidSpells.js
@@ -2,19 +2,19 @@ import helpFetch from "../../../helpers/blizFetch-helpers/endpointFetchesBliz";
 import GameSpell from "../../../Models/GameSpell.js";
 
 export default async function retriveValidSpells(spellArray) {
-    if (!(Array.isArray(spellArray))) {
+    if (!Array.isArray(spellArray)) {
         throw new TypeError("The argument have to be type of Array");
     }
     if (spellArray.some((id) => typeof id !== "string" && typeof id !== "number")) {
-        throw new TypeError("All ids must be strings or numbers")
+        throw new TypeError("All ids must be strings or numbers");
     }
 
     const spellArraySet = new Set(spellArray);
     const data = await GameSpell.find({ _id: { $in: Array.from(spellArraySet) } }).lean();
-    
+
     const existingSet = data.reduce((acc, entry) => {
-      acc.add(String(entry._id));
-      return acc;
+        acc.add(String(entry._id));
+        return acc;
     }, new Set());
 
     for (const existingId of existingSet) spellArraySet.delete(existingId);
@@ -24,20 +24,16 @@ export default async function retriveValidSpells(spellArray) {
 
         const newEntry = {};
 
-
         if (newData === null || !newData) {
-            newEntry._id = needsRetrivingID,
-            newEntry.name = newData
+            ((newEntry._id = needsRetrivingID), (newEntry.name = newData));
         } else if (newData.name) {
             newEntry._id = needsRetrivingID;
             newEntry.name = newData.name;
             newEntry.description = newData.description;
             newEntry.media = newData.media;
-
         }
 
         const newDBEntry = new GameSpell(newEntry);
-        await newDBEntry.save()
+        await newDBEntry.save();
     }
-
 }
