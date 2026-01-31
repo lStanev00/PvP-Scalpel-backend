2f8cd0bf3d4f485ea61cf2c79fe2a24098397dd8
lStanev00 <lychezarcho@abv.bg>
2026-01-31 11:16:50 +0200

    demo moc

--- src/controllers/route_logic/gameDataCTRL/retriveValidSpells.js
+++ src/controllers/route_logic/gameDataCTRL/retriveValidSpells.js
@@ -1,3 +1,4 @@
+import helpFetch from "../../../helpers/blizFetch-helpers/endpointFetchesBliz";
 import GameSpell from "../../../Models/GameSpell.js";
 
 export default async function retriveValidSpells(spellArray) {
@@ -8,6 +9,35 @@ export default async function retriveValidSpells(spellArray) {
         throw new TypeError("All ids must be strings or numbers")
     }
 
-    const data = await GameSpell.find({ _id: { $in: spellArray } }).lean();
+    const spellArraySet = new Set(spellArray);
+    const data = await GameSpell.find({ _id: { $in: Array.from(spellArraySet) } }).lean();
     
+    const existingSet = data.reduce((acc, entry) => {
+      acc.add(String(entry._id));
+      return acc;
+    }, new Set());
+
+    for (const existingId of existingSet) spellArraySet.delete(existingId);
+
+    for (const needsRetrivingID of spellArraySet) {
+        const newData = helpFetch.getSpellById(needsRetrivingID);
+
+        const newEntry = {};
+
+
+        if (newData === null || !newData) {
+            newEntry._id = needsRetrivingID,
+            newEntry.name = newData
+        } else if (newData.name) {
+            newEntry._id = needsRetrivingID;
+            newEntry.name = newData.name;
+            newEntry.description = newData.description;
+            newEntry.media = newData.media;
+
+        }
+
+        const newDBEntry = new GameSpell(newEntry);
+        await newDBEntry.save()
+    }
+
 }
