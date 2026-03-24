cd88dddaf30772fd2e3a64c4142525e3d0b2c630
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-03-24 16:30:34 +0200

    remvoe import

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,7 +1,7 @@
 import startServices from "../services/servicesMain.js"
 import { DBconnect } from "../helpers/mongoHelper.js";
 import connectRedis from "../helpers/redis/connectRedis.js";
-import startRedisCharSubscriber from "../helpers/redis/charSubscriber.js";
+// import startRedisCharSubscriber from "../helpers/redis/charSubscriber.js";
 import { delay } from "../helpers/startBGTask.js";
 
 await DBconnect(true);
