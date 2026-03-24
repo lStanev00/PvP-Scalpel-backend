0aaf2fbb651a259b465b7479a208243921ee6b6d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-03-24 22:30:35 +0200

    initial cache is in different process to release any allocated runtime ram

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,17 +1,14 @@
-// version: 1.3.0
-import startServices from "../services/servicesMain.js"
+// version: 1.3.1
+import startServices from "../services/servicesMain.js";
 import { DBconnect } from "../helpers/mongoHelper.js";
 import connectRedis from "../helpers/redis/connectRedis.js";
 // import startRedisCharSubscriber from "../helpers/redis/charSubscriber.js";
 import { delay } from "../helpers/startBGTask.js";
 
-await DBconnect(true);
-await connectRedis(true);
+await DBconnect();
+await connectRedis();
 // await startRedisCharSubscriber();
 
-(async () => {
+await delay(1500);
 
-    await delay(1500);
-    
-    await startServices();
-})()
+await startServices();
