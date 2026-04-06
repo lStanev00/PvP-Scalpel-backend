07ccaabbbfdf47772be05765ce51b36a87630e92
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-04-06 15:21:53 +0300

    refacturing

--- src/workers/jobQueue/JQORefacture.js
+++ src/workers/jobQueue/JQORefacture.js
@@ -6,37 +6,43 @@ import {
 import JQOLog from "./JQOLoog.js";
 import QueueWorker from "./jobWorkers/classJobWorker.js";
 import threadBoot from "../../helpers/threadBoot.js";
-import { redisCache } from "../../helpers/redis/connectRedis.js";
+import { delay } from "../../helpers/startBGTask.js";
 
 await threadBoot(true);
-
-const key = "JobQueue";
 
 const QueueWorker1 = new QueueWorker("QueueWorker1");
 const QueueWorker2 = new QueueWorker("QueueWorker2");
 
 let draining = false;
-const subClone = redisCache.duplicate();
-await subClone.connect();
-await subClone.pSubscribe(`__keyspace@0__:${key}`, async (event, channel) => {
-    if (!event.includes("push") || draining) return;
-    draining = true;
+let currentJobInfo = null;
+JQOLog.info("BOOTED");
 
-    //start the job queueing
+process.on("message", (msg) => {
+    console.log("received:", msg);
 
+    if (msg === "stop") {
+        while (currentJobInfo !== null) delay(300).catch()
+        process.exit(0);
+    }
+});
+
+startQueue();
+
+async function startQueue() {
     try {
+        draining = true;
         while ((await getJobQueueSize()) !== 0) {
             // run till queue is drained
             //get curent job
-            const currentJobInfo = (await getJobQueueEntries()).shift();
+            currentJobInfo = (await getJobQueueEntries()).shift();
             await deleteJobQueueEntry(currentJobInfo);
             if (!currentJobInfo) {
                 JQOLog.warn("There's a falsy job:" + currentJobInfo);
                 continue;
             }
-    
+
             const { type, data } = currentJobInfo;
-    
+
             if (type === "retrieveCharacter") {
                 await QueueWorker1.retrieveCharacter(data);
             } else if (type === "bulkRetrieveCharacter") {
@@ -47,9 +53,8 @@ await subClone.pSubscribe(`__keyspace@0__:${key}`, async (event, channel) => {
                 if (data.length <= 2) {
                     for (const jobData of data) await QueueWorker2.retrieveCharacter(jobData);
                     continue;
-    
                 }
-    
+
                 for (let i = 0; i < data.length; i++) {
                     const jobData = data[i];
                     if (i <= Math.floor(data.length / 2)) {
@@ -59,12 +64,11 @@ await subClone.pSubscribe(`__keyspace@0__:${key}`, async (event, channel) => {
                     }
                 }
             }
+            currentJobInfo = null;
         }
-
     } finally {
         draining = false;
     }
-});
+};
 
-JQOLog.info("BOOTED");
 // Hello buddy how're ya doing?
