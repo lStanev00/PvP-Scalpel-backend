0ba3355fa66dba1dcc709ae139d4aa54cda22a96
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-04-07 11:28:46 +0300

    returning signed urlk for m the server

--- src/caching/CDNCache/CDN/cdn.config.js
+++ src/caching/CDNCache/CDN/cdn.config.js
@@ -1,9 +1,20 @@
 import "dotenv/config";
 
-export const CDNURI =
-    "http://" +
-    process.env.CDN_PRIVATE_DOMAIN +
-    ":" +
-    process.env.CDN_PORT;
+export const CDNURI = "http://" + process.env.CDN_PRIVATE_DOMAIN + ":" + process.env.CDN_PORT;
 
 export const CDNAUTH = process.env.JWT_CDN_PUBLIC;
+
+export async function retriveCDNLink(keyId) {
+    const path = `${CDNURI.endsWith("/") ? "" : "/"}presign/download`
+    const response = await fetch(CDNURI + path, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${CDNAUTH}`,
+        },
+        body: JSON.stringify({
+            keyId
+        })
+    });
+
+    return await response.json()
+}
