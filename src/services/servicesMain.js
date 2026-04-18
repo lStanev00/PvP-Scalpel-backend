df1edb29f53077b10b156051acdfc3a744e33244
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-04-18 12:18:25 +0300

    caceh

--- src/services/servicesMain.js
+++ src/services/servicesMain.js
@@ -8,13 +8,13 @@ const jobQueue = new JobQueue();
 export default async function startServices() {
     await jobQueue.initialize();
 
-    // let warmupFinished = false;
-    // const cacheWormupTask = fork("src/workers/initialChace/workerInitialCache.js");
-    // cacheWormupTask.on("exit",() => {
-    //     warmupFinished=true;
-    // });
-    // while (warmupFinished !== true) await delay(1000);
-    // console.info("[Cache] Initial cache warmup finished.");
+    let warmupFinished = false;
+    const cacheWormupTask = fork("src/workers/initialChace/workerInitialCache.js");
+    cacheWormupTask.on("exit",() => {
+        warmupFinished=true;
+    });
+    while (warmupFinished !== true) await delay(1000);
+    console.info("[Cache] Initial cache warmup finished.");
 
     workerUpdateRealm();
     workerPatchGuildMembersData();
