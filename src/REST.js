c33e383abede4bbbdd40328ae3a480e4bb158a40
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-05-16 12:23:17 +0300

    data efficiency change

--- src/REST.js
+++ src/REST.js
@@ -1,4 +1,4 @@
-// version: 0.0.6
+// version: 0.0.7
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,4 +1,4 @@
-// version: 1.8.13
+// version: 1.8.14
 import { redisCharacterCacheTTL } from "../helpers/redis/connectRedis.js";
 import threadBoot from "../helpers/threadBoot.js";
 import startServices from "../services/servicesMain.js";
