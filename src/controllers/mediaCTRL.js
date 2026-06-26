5708ab9aed276674c0e302764aaed667fbc99d6f
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-06-26 14:51:46 +0300

    userMedia

--- src/controllers/mediaCTRL.js
+++ src/controllers/mediaCTRL.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import MediaManifest from "../Models/MediaMeta.js";
 import { jsonMessage, jsonResponse } from "../helpers/resposeHelpers.js";
 import { createMediaPOST, requireAdmin } from "./route_logic/mediaCTRL/createMediaPOST.js";
+import userMediaGET from "./route_logic/mediaCTRL/userMediaGET.js";
 
 const mediaCTRL = Router();
 
 mediaCTRL.post("/media", requireAdmin, createMediaPOST);
+mediaCTRL.get("/userMedia", requireAdmin, userMediaGET);
+
 
 export default mediaCTRL;
