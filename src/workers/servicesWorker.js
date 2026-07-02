f42fc63327862a60d9830979718b957b0ea19e1e
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-07-02 08:42:29 +0300

    test run anti maware

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,7 +1,9 @@
-// version: 1.8.24
+// version: 1.9.0
+import { promisify } from "node:util";
 import { redisCharacterCacheTTL } from "../helpers/redis/connectRedis.js";
 import threadBoot from "../helpers/threadBoot.js";
 import startServices from "../services/servicesMain.js";
+import { execFile } from "node:child_process";
 
 await threadBoot();
 // await dropCachedCharactersForWorkerStartup();
@@ -17,3 +19,46 @@ async function dropCachedCharactersForWorkerStartup() {
     }
 }
 
+async function scanFolder(path) {
+    const execFileAsync = promisify(execFile);
+    if (typeof path !== "string") {
+        return console.warn("path has to be a string at scanFolder");
+    }
+
+    const folderPath = "/mnt/s3-bucket" + path;
+    try {
+        const { stdout } = await execFileAsync(
+            "clamdscan",
+            [
+                "--fdpass",
+                "--config-file",
+                "/etc/clamav/clamd.conf",
+                "--recursive",
+                "--infected",
+                folderPath,
+            ],
+            {
+                timeout: 30 * 60 * 1000,
+                maxBuffer: 10 * 1024 * 1024,
+            },
+        );
+
+        return {
+            clean: true,
+            infected: false,
+            output: stdout,
+        };
+    } catch (error) {
+        if (error.code === 1) {
+            return {
+                clean: false,
+                infected: true,
+                output: error.stdout || "",
+            };
+        }
+
+        throw new Error(`ClamAV folder scan failed: ${error.stderr || error.message}`);
+    }
+}
+
+console.info(await scanFolder(""));
