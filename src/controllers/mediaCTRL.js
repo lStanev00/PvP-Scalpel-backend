633461ea269669f248758ac319b2eede880450e5
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-06 16:54:11 +0300

    ship ids for the sitemap

--- src/controllers/mediaCTRL.js
+++ src/controllers/mediaCTRL.js
@@ -20,8 +20,29 @@ mediaCTRL.get("/userMedia", requireAdmin, userMediaGET);
 // specifical controllers for the videos;
 mediaCTRL.get("/videos/user", getUserVideos);
 mediaCTRL.get("/videos", getVideos);
+mediaCTRL.get("/videosIDs", getVideosIDs);
 mediaCTRL.get("/video/:videoID", getVideo);
 mediaCTRL.delete("/video/:videoID", deleteVideo);
+
+async function getVideosIDs(_, res) {
+    try {
+
+        const idsArr = (
+            await MediaMeta.distinct("_id", {
+                type: "video",
+                state: "done",
+                censored: false,
+                isPrivate: false,
+                quarantined: false,
+            })
+        ).map(String);
+
+        return jsonResponse(res, 200, idsArr);
+    } catch (error) {
+        console.error(error);
+        return jsonResponse(res, 500);
+    }
+}
 
 async function deleteVideo(req, res) {
     const { videoID } = req.params;
