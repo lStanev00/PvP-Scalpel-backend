895a87299c5aeb43b62a3df3f228cd6fb65899f1
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-06 13:17:55 +0300

    caching changes

--- src/bot/zugee.js
+++ src/bot/zugee.js
@@ -1,4 +1,4 @@
-// version: 1.1.50
+// version: 1.1.51
 
 // This is a discord bot
 // the name of the file is the name of the bot
--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,4 +1,4 @@
-// version: 1.9.47
+// version: 1.9.48
 import { redisCharacterCacheTTL } from "../helpers/redis/connectRedis.js";
 import threadBoot from "../helpers/threadBoot.js";
 import startServices from "../services/servicesMain.js";
