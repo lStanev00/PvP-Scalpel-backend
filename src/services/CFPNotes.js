f89c3da0f475dbb26c735011c4d17ec7d580587d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-12 14:56:42 +0300

    new class changes announcement for discrod

---
+++ src/services/CFPNotes.js
@@ -0,0 +1,33 @@
+import { redisCache } from "../helpers/redis/connectRedis.js";
+import getCache from "../helpers/redis/getterRedis.js";
+import threadBoot from "../helpers/threadBoot.js";
+import analyzePNotes from "./Service-Helpers/CFPNotes/analyzePNotes.js";
+import generatePNotesSummaryCard from "./Service-Helpers/CFPNotes/generatePNotesSummaryCard.js";
+import { getLatestPNotes } from "./Service-Helpers/CFPNotes/getLatesPNotes.js";
+import getPostContent from "./Service-Helpers/CFPNotes/getPostContent.js";
+
+await threadBoot(true);
+
+const HASH_NAME = "CFPNotes";
+
+const latestPNotes = await getLatestPNotes()
+// if (!latestPNotes?.id) process.exit(1);
+
+const latestCache = await getCache(HASH_NAME);
+// if (latestCache === latestPNotes.id) process.exit(0);
+
+const postContent = await getPostContent(latestPNotes);
+const analysis = await analyzePNotes(postContent);
+const cardBuffer = await generatePNotesSummaryCard(analysis, postContent);
+
+const publishMsg = {
+    title: postContent.title,
+    url: postContent.url,
+    cardBuffer
+}
+
+await redisCache.publish("annoDiscord:newClassChanges", JSON.stringify(publishMsg));
+
+// console.info(JSON.stringify({ analysis, card }, null, 4));
+// console.info(JSON.stringify(analysis, null, 4));
+process.exit(0);
