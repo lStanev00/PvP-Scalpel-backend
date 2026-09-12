9f9d638141fcf74059d5703b6f2b5744858d72e4
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-12 17:26:53 +0300

    prod readiness

--- src/workers/checkForPatchNotes/checkForPatchNotes.js
+++ src/workers/checkForPatchNotes/checkForPatchNotes.js
@@ -15,7 +15,7 @@ export default async function checkForPatchNotes() {
         });
 
         // while (exited !== true) await delay(3000);
-        await delay(2147483647); // max 24.8 days
+        await delay(3600000); // 1 hr delay
     }
     // src\services\updateRealms.js
 }
