6fe32facc40331c229c1af5d31d86c74d3c06d64
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-21 09:42:37 +0300

    timeout adjusted

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,4 +1,4 @@
-// version: 1.9.62
+// version: 1.9.63
 import { redisCharacterCacheTTL } from "../helpers/redis/connectRedis.js";
 import threadBoot from "../helpers/threadBoot.js";
 import startServices from "../services/servicesMain.js";
