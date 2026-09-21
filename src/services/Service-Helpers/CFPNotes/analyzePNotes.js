1e7633b8c7c06f77724067e3f6b0997e3a2d5acf
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-21 10:02:19 +0300

    CTX increase

--- src/services/Service-Helpers/CFPNotes/analyzePNotes.js
+++ src/services/Service-Helpers/CFPNotes/analyzePNotes.js
@@ -5,7 +5,7 @@ const DEFAULT_OLLAMA_BASE_URL = "http://localhost:11434";
 // const DEFAULT_OLLAMA_MODEL = "qwen3:8b";
 const DEFAULT_OLLAMA_MODEL = "gemma4:e4b";
 const DEFAULT_TIMEOUT_MS = 2 * 60 * 1000 * 2;
-const DEFAULT_CONTEXT_LENGTH = 8192;
+const DEFAULT_CONTEXT_LENGTH = 16384;
 
 const CHANGE_TYPES = Object.freeze([
     "buff",
