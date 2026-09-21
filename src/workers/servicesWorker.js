28de2ce6b55703a1ef41fbe8ed955ef59d0b732a
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2026-09-21 08:45:29 +0300

    no warmup

--- src/workers/servicesWorker.js
+++ src/workers/servicesWorker.js
@@ -1,4 +1,4 @@
-// version: 1.9.58
+// version: 1.9.59
 import { redisCharacterCacheTTL } from "../helpers/redis/connectRedis.js";
 import threadBoot from "../helpers/threadBoot.js";
 import startServices from "../services/servicesMain.js";
